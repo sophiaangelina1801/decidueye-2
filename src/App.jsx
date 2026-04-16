90ab899626630ad960c3c73aed26d9962d07302e
Soso <sophiaangel2307@gmail.com>
2026-04-16 00:42:35 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+import deci from './assets/header deci.png'
+import folha from './assets/folha.png'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main>
+      <header>
+        <img className='deci' src={deci} alt="" />
+        <h1> Decidueye </h1>
+        <h2>N°0724</h2>
+      </header>
+      <button>
+        <img className='folha' src={folha} alt="" />
+        Grama
+      </button>
+      <h3>Decidueye consegue se mover enquanto oculta completamente sua presença dos outros. Com a distração de um oponente, Decidueye aproveita a oportunidade para atacá-lo de surpresa.</h3>
+      <h4>______________________________________________</h4>
+      <h5> ⚖︎  PESO</h5>
+
+    </main>
+  
+  )
+}
